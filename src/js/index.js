a92cff74dab76d2c3f1d05c4cb017e317015ce08
Roberto Meran <roberto.meran@contractor.temenos.com>
2020-07-10 18:21:15 -0400

    Fix an issue regarding the navigation to the results page

--- src/js/index.js
+++ src/js/index.js
@@ -2,31 +2,33 @@
 import Search from "./models/Search";
 import * as searchView from "./views/searchView";
 
+const resultsPagePath = "/results.html";
 const searchForm = document.querySelector(".search-submit");
 
 let state = {};
 
 const controlSearch = async () => {
-    const searchInput = document.querySelector(".input-search").value;    
+    const searchInput = document.querySelector(".input-search").value;
     state.search = new Search(searchInput);
 
-    try{
+    try {
         await state.search.searchMovie();
-        console.log(state.search.results);
-        navigateResultsPage();
-        searchView.displayResults(state.search.results);
-        
-    }catch(err){
+        navigateResultsPage(state.search.results);
+
+    } catch (err) {
         alert(err);
     }
-    
+
 }
 
-const navigateResultsPage = () => {
-    window.location.href = "./results.html";
+const navigateResultsPage = results => {
+    history.pushState({ results }, "", resultsPagePath);
+    window.location.href = resultsPagePath;
 }
 
 searchForm.addEventListener("submit", e => {
     e.preventDefault();
     controlSearch();
 });
+
+if (window.location.pathname === resultsPagePath) searchView.displayResults(history.state.results);
